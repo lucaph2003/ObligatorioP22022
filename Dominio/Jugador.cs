d19f68ccedec7136d43f619939ed612a058550ca
lucaph2003 <lucapodesta47@gmail.com>
2022-09-22 14:02:50 -0300

    Por fin

--- Dominio/Jugador.cs
+++ Dominio/Jugador.cs
@@ -7,32 +7,37 @@ namespace Dominio
     public class Jugador : Persona 
     {
         #region Atributos Jugador
-        public int dorsal { get; set; }
+        public int id { get; set; }
+        public string dorsal { get; set; }
         public DateTime fechaNacimiento { get; set; }
-        public decimal altura { get; set; }
-        public int pieHabil { get; set; }
+        public double altura { get; set; }
+        public string pieHabil { get; set; }
         public int valorMercado { get; set; }
+        public string moneda { get; set; }
         public Pais pais { get; set; }
-        public int puesto { get; set; }
+        public string puesto { get; set; }
         public string categoria { get; set; }
         public static int montoCategoria { get; set; }
+        public static int contador { get; set; } = 872;
         #endregion
         #region Metodos Jugador
 
-        public Jugador(string pNombre,string pApellido,int pDorsal,DateTime pFechaNacimiento,decimal pAltura,int pPieHabil,int pValorMercado,Pais pPais,int pPuesto):base(pNombre,pApellido)
+        public Jugador(int pId, string pDorsal, string pNombreCompleto,DateTime pFechaNacimiento,double pAltura,string pPieHabil,int pValorMercado,string pMoneda,Pais pPais,string pPuesto):base(pNombreCompleto)
         {
+            this.id = contador++;
             this.dorsal = pDorsal;
             this.fechaNacimiento = pFechaNacimiento;
             this.altura = pAltura;
             this.pieHabil = pPieHabil;
             this.valorMercado = pValorMercado;
+            this.moneda = pMoneda;
             this.pais = pPais;
             this.puesto = pPuesto;
             CalcularCategoria();
         }
         public override void Validar()
         {
-            if(this.nombre != null &&this.apellido != null&&this.dorsal >= 0 && this.fechaNacimiento != null && this.altura > 0 && this.pieHabil > 0 && this.valorMercado >= 0 && this.pais != null && this.puesto != 0)
+            if(this.nombreCompleto != null &&this.dorsal !=null && this.fechaNacimiento != null && this.altura > 0.0 && this.pieHabil != null && this.valorMercado >= 0 && this.pais != null && this.puesto != null)
             {
 
             }
--- Dominio/Persona.cs
+++ Dominio/Persona.cs
@@ -7,16 +7,14 @@ namespace Dominio
     public abstract class Persona : IValidable
     {
         #region Atributos Persona
-        public int id { get; set; }
-        public string nombre { get; set; }
-        public string apellido { get; set; }
-        public static int contador = 0;
+        
+        public string nombreCompleto { get; set; }
+        
         #endregion
         #region Metodos Persona
-        public Persona(string pNombre,string pApellido)
+        public Persona(string pNombreCompleto)
         {
-            this.nombre = pNombre;
-            this.apellido = pApellido;
+            this.nombreCompleto = pNombreCompleto;
         }
         public abstract void Validar();
 

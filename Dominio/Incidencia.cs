a4dbbeedcaeb35572656b618e9bca3c69d180ac6
lucaph2003 <lucapodesta47@gmail.com>
2022-09-29 19:14:52 -0300

    regions incidencia

--- Dominio/Incidencia.cs
+++ Dominio/Incidencia.cs
@@ -6,20 +6,36 @@ namespace Dominio
 {
     public class Incidencia
     {
+        #region Atributos
         public string incidencia { get; set; }
         public int minuto { get; set; }
         public Jugador jugador { get; set; }
+        #endregion
 
+        #region Metodos Incidencia
+
+        #region Constructores
         public Incidencia(string pIncidencia,int pMinuto,Jugador pJugador)
         {
             this.incidencia = pIncidencia;
             this.minuto = pMinuto;
             this.jugador = pJugador;
         }
+        public Incidencia()
+        {
+            this.incidencia = "Sin definir";
+            this.minuto = 0;
+            this.jugador = null;
+        }
+        #endregion
 
+        #region Override y Compare
         public override string ToString()
         {
             return $"Incidencia: {incidencia}" + "\n" + $"Minuto: {minuto}" + "\n" + $"Nombre: {jugador.nombreCompleto}";
         }
+        #endregion
+
+        #endregion
     }
 }
